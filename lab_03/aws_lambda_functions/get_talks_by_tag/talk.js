e728c4926c5bcac98f53d7641e86f6372758d3f7
mnau23 <perico.emanuele@gmail.com>
2020-05-29 18:21:05 +0200

    Update talk.js files

--- lab_03/aws_lambda_functions/get_talks_by_tag/talk.js
+++ lab_03/aws_lambda_functions/get_talks_by_tag/talk.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const talk_schema = new mongoose.Schema({
+    main_speaker: String,
     title: String,
-    url: String,
     details: String,
-    main_author: String
-}, { collection: 'tedx_data' });
+    url: String
+}, {collection: 'tedx_data'});
 
 module.exports = mongoose.model('talk', talk_schema);
--- lab_03/aws_lambda_functions/get_talks_by_title/handler.js
+++ lab_03/aws_lambda_functions/get_talks_by_title/handler.js
@@ -14,7 +14,7 @@ module.exports.get_by_title = (event, context, callback) => {
         callback(null, {
                     statusCode: 500,
                     headers: { 'Content-Type': 'text/plain' },
-                    body: 'Could not fetch the talks. Tag is null.'
+                    body: 'Could not fetch the talks. Title is null.'
         })
     }
     
